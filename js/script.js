cc547bb114d7cdad6704d10f12872cc5f441349b
mello2118 <jeditimelord1018@gmail.com>
2025-10-08 14:17:14 -0500

    idk why its not working
    maps is confusing

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-      var map;
       var marker;
 
       function initMap(){
-      map = new google.maps.Map(document.getElementById('map'), {
+      var map = new google.maps.Map(document.getElementById('map'), {
       center:{lat: 10.2926, lng: 123.9022},
       zoom: 8
       });
+      google.maps.event.addDomListener(window, 'load', initMap);
+      
       marker = new google.maps.Marker({
         position:{lat: 10.2926, lng: 123.9022},
         map: map,
