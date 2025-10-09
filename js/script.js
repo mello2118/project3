daf7d93ad08dbe3187df48dc5f18bb57f9d6b790
mello2118 <jeditimelord1018@gmail.com>
2025-10-08 20:49:29 -0500

    fixing map
    did some rewatching and asked a friend who is good with this stuff and guided me to what to do, by creating a proper height and width

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-      var marker;
-
       function initMap(){
-      var map = new google.maps.Map(document.getElementById('map'), {
+      const map = new google.maps.Map(document.getElementById"map"), {
       center:{lat: 10.2926, lng: 123.9022},
       zoom: 8
       });
       google.maps.event.addDomListener(window, 'load', initMap);
       
-      marker = new google.maps.Marker({
+      const marker = new google.maps.Marker({
         position:{lat: 10.2926, lng: 123.9022},
         map: map,
+        title: "Cebu City",
       });
     }
